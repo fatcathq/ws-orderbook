4f6df6a1985c3f2205333761256556e0af8c950c
Themis Papameletiou <themicp@gmail.com>
2019-01-08 22:39:42 +0200

    [STREAMER]: Move BittrexStreamer and streamer factory to separate files

--- src/streamer.ts
+++ src/streamer.ts
@@ -1,24 +1,23 @@
-import BittrexConnection from './bittrexconnection'
-import { marketFactory, MarketName } from './market'
+import { Market, MarketName } from './market'
 
-abstract class Streamer {
+export default abstract class Streamer {
     protected markets: {[id: string]: any} = {}
     protected conn: any
     protected exchange: any
 
     abstract setupConn(): void
     abstract subscribeToMarket(market: MarketName): void
-    abstract getInitialState(market: MarketName): void
 
-    constructor (public readonly exchangeName: string) {}
+    constructor (public readonly exchangeName: string) {
+        this.setupConn()
+    }
 
     public market(market: MarketName) {
         if (!this.haveMarket(market)) {
             // create market now
-            this.markets[market] = marketFactory(this.exchangeName, market)
+            this.markets[market] = new Market(market)
             this.conn
                 .ready()
-                .then(() => this.getInitialState(market))
                 .then(() => this.subscribeToMarket(market))
         }
         return this.markets[market]
@@ -26,45 +25,5 @@ abstract class Streamer {
 
     public haveMarket(market: MarketName): boolean {
         return this.markets.hasOwnProperty(market)
-    }
-}
-
-class BittrexStreamer extends Streamer {
-    constructor () {
-        super('bittrex')
-
-        this.setupConn()
-    }
-
-    setupConn () {
-        this.conn = new BittrexConnection()
-
-        this.conn.on('updateExchangeState', (update: any) => {
-            const market = update.MarketName
-            if (this.haveMarket(market)) {
-                this.markets[market].onUpdateExchangeState(update)
-            }
-        })
-    }
-
-    subscribeToMarket(market: MarketName) {
-        return this.conn.call('SubscribeToExchangeDeltas', market)
-    }
-
-    getInitialState(market: MarketName) {
-        if (this.haveMarket(market)) {
-            return this.conn
-                .call('QueryExchangeState', market)
-                .then(this.markets[market].onInitialState)
-        }
-    }
-}
-
-export default function streamerFactory(exchangeName: string) {
-    switch (exchangeName) {
-    case 'bittrex':
-        return new BittrexStreamer()
-    default:
-        throw new Error(`No streamer for ${exchangeName}`)
     }
 }
