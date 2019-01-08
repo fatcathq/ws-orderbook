32a6c46252d8fca5b588de6186b3d24a6ed803f0
Themis Papameletiou <themicp@gmail.com>
2019-01-08 22:42:19 +0200

    [BITTREX]: Bittrex streamer with new types and formatting of websocket messages

---
+++ src/bittrexstreamer.ts
@@ -0,0 +1,68 @@
+import Streamer from './streamer'
+import BittrexConnection from './bittrexconnection'
+import { OrderBookState, OrderBookStateUpdate } from './orderbook'
+import { MarketName } from './market'
+
+type BittrexOrder = {
+    Rate: number,
+    Quantity: number
+}
+
+interface BittrexOrderUpdate extends BittrexOrder {
+    Type: 0 | 1 | 2
+}
+
+type BittrexOrderBookState = {
+    Buys: Array<BittrexOrder>,
+    Sells: Array<BittrexOrder>,
+}
+
+type BittrexOrderBookStateUpdate = {
+    MarketName: MarketName,
+    Buys: Array<BittrexOrderUpdate>,
+    Sells: Array<BittrexOrderUpdate>,
+}
+
+const formatOrderKeys = ({Rate, Quantity}: BittrexOrder) => ({rate: Rate, quantity: Quantity})
+const formatOrderUpdateKeys = ({Type, Rate, Quantity}: BittrexOrderUpdate) => ({type: Type, rate: Rate, quantity: Quantity})
+
+export default class BittrexStreamer extends Streamer {
+    constructor () {
+        super('bittrex')
+    }
+
+    setupConn () {
+        this.conn = new BittrexConnection()
+
+        this.conn.on('updateExchangeState', (update: BittrexOrderBookStateUpdate) => {
+            const market = update.MarketName
+            if (this.haveMarket(market)) {
+                const orderBookUpdate: OrderBookStateUpdate = {
+                    asks: update.Sells.map(formatOrderUpdateKeys),
+                    bids: update.Buys.map(formatOrderUpdateKeys),
+                }
+
+                this.markets[market].onUpdateExchangeState(orderBookUpdate)
+            }
+        })
+    }
+
+    subscribeToMarket(market: MarketName) {
+        return this.getInitialState(market)
+            .then(() => this.conn.call('SubscribeToExchangeDeltas', market))
+    }
+
+    getInitialState(market: MarketName) {
+        if (this.haveMarket(market)) {
+            return this.conn
+                .call('QueryExchangeState', market)
+                .then((state: BittrexOrderBookState) => {
+                    const orderBookState: OrderBookState = {
+                        asks: state.Sells.map(formatOrderKeys),
+                        bids: state.Buys.map(formatOrderKeys)
+                    }
+                    this.markets[market].onInitialState(orderBookState)
+                })
+        }
+    }
+}
