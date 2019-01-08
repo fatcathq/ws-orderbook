dbf63e133d376ca32df1ff6de028bdb22ec7fdbf
Themis Papameletiou <themicp@gmail.com>
2019-01-08 22:47:33 +0200

    [MARKET]: Remove Bittrex-specific Market, rename Buys->bids, Sells->asks, Type->type

--- src/market.ts
+++ src/market.ts
@@ -1,69 +1,55 @@
+import { OrderBookState, OrderBookStateUpdate } from './orderbook'
 import BidOrderBook from './bidorderbook'
 import AskOrderBook from './askorderbook'
 import EventEmitter from 'events'
 
 type MarketName = string
 
-abstract class Market extends EventEmitter {
+class Market extends EventEmitter {
     public name: MarketName
     public bids: BidOrderBook
     public asks: AskOrderBook
-    abstract onUpdateExchangeState(update: any): void
 
     constructor(name: MarketName) {
         super()
-
-        this.onInitialState = this.onInitialState.bind(this)
-        this.onUpdateExchangeState = this.onUpdateExchangeState.bind(this)
 
         this.name = name
         this.bids = new BidOrderBook()
         this.asks = new AskOrderBook()
     }
 
-    onInitialState(state: any) {
-        let { Sells, Buys } = state
+    onInitialState(state: OrderBookState) {
+        let { asks, bids } = state
 
         // type 0 means new order
         const addTypeZero = (order: any) => {
             return {
-                Type: 0,
+                type: 0,
                 ...order
             }
         }
-        Sells = Sells.map(addTypeZero)
-        Buys = Buys.map(addTypeZero)
-        this.onUpdateExchangeState({
-            Sells,
-            Buys
-        })
-    }
-}
 
-class BittrexMarket extends Market {
-    onUpdateExchangeState(update: any) {
-        update.Sells.forEach(this.asks.onOrderEvent)
-        if (update.Sells.length > 0) {
+        const update: OrderBookStateUpdate = {
+            asks: asks.map(addTypeZero),
+            bids: bids.map(addTypeZero)
+        }
+        this.onUpdateExchangeState(update)
+    }
+
+    onUpdateExchangeState(update: OrderBookStateUpdate) {
+        update.asks.forEach(this.asks.onOrderEvent)
+        if (update.asks.length > 0) {
             this.emit('askUpdate', this)
         }
 
-        update.Buys.forEach(this.bids.onOrderEvent)
-        if (update.Buys.length > 0) {
+        update.bids.forEach(this.bids.onOrderEvent)
+        if (update.bids.length > 0) {
             this.emit('bidUpdate', this)
         }
     }
 }
 
-export default function marketFactory (exchangeName: string, marketName: MarketName) {
-    switch (exchangeName) {
-    case 'bittrex':
-        return new BittrexMarket(marketName)
-    default:
-        throw new Error(`No available market for ${exchangeName}`)
-    }
-}
-
 export {
-    marketFactory,
+    Market,
     MarketName
 }
