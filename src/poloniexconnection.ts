cce544c859428eba589a3f74e8e6cf3aad6a5bdf
Themis Papameletiou <themicp@gmail.com>
2019-01-08 22:49:58 +0200

    PoloniexConnection class

---
+++ src/poloniexconnection.ts
@@ -0,0 +1,47 @@
+import WebSocket from 'ws'
+import Connection from './connection'
+
+const HEARTBEAT_TIMEOUT_MS = 1500
+
+export default class PoloniexConnection extends Connection {
+    private client: WebSocket
+    private aliveTimeout: NodeJS.Timer | null
+
+    constructor() {
+        super()
+
+        this.aliveTimeout = null
+
+        this.client = new WebSocket('wss://api2.poloniex.com')
+        this.client.on('open', () => {
+            this.alive()
+            this.connectionOpened()
+        })
+        this.client.on('message', this.onMessage)
+    }
+
+    call(method: string, options: any) {
+        this.client.send(JSON.stringify(Object.assign({command: method}, options)))
+    }
+
+    private onMessage = (messageString: any): void => {
+        this.alive()
+        if (messageString === '[1010]') { // Heartbeat
+            return
+        }
+
+        this.emit('updateExchangeState', JSON.parse(messageString))
+    }
+
+    private alive() {
+        if (this.aliveTimeout) {
+            clearTimeout(this.aliveTimeout)
+        }
+
+        this.aliveTimeout = setTimeout(() => {
+            // TODO: Retry connection
+            this.connectionFailed()
+            throw new Error('WebSocket connection with Poloniex died.')
+        }, HEARTBEAT_TIMEOUT_MS)
+    }
+}
