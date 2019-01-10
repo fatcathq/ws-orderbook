1d64dd8fe5b6cfd881d90083f28d1c54f3b4955c
Themis Papameletiou <themicp@gmail.com>
2019-01-10 12:38:41 +0200

    [POLONIEX_STREAMER]: Handle initial state and order update on different methods

--- src/poloniexstreamer.ts
+++ src/poloniexstreamer.ts
@@ -42,53 +42,60 @@ export default class PoloniexStreamer extends Streamer {
         throw new Error(`Unknown channel: ${channelId}.`)
       }
 
-      this.onMarketUpdate(market, message[2])
+      if (message[2][0][0] === 'i') {
+        this.onInitialState(market, message[2])
+      } else if (message[2][0][0] === 'o') {
+        this.onOrderUpdate(market, message[2])
+      } else {
+        logger.warn(`Unrecognized first entry of Poloniex payload: ${JSON.stringify(message[2][0])}`)
+      }
     })
   }
 
-  private onMarketUpdate (market: string, payload: any): void {
+  private onInitialState (market: MarketName, payload: Array<InitialState>): void {
     if (this.haveMarket(market)) {
-      const messageType = payload[0][0]
-      if (messageType === 'i') {
-        const [asks, bids] = payload[0][1].orderBook
-        const orderBook: OrderBookState = { asks: [], bids: [] }
+      const [asks, bids] = payload[0][1].orderBook
+      const orderBook: OrderBookState = { asks: [], bids: [] }
 
-        for (const rate in asks) {
-          orderBook.asks.push({
-            rate: +rate,
-            quantity: +asks[rate]
-          })
-        }
-
-        for (const rate in bids) {
-          orderBook.bids.push({
-            rate: +rate,
-            quantity: +bids[rate]
-          })
-        }
-
-        this.markets[market].onInitialState(orderBook)
-      } else {
-        const orderBookUpdate: OrderBookStateUpdate = { asks: [], bids: [] }
-
-        for (const updateEntry of payload) {
-          if (updateEntry[0] !== 'o') {
-                        // might be 't' for trades
-            continue
-          }
-          const orderSide = +updateEntry[1] === 0 ? 'asks' : 'bids'
-          const rate = +updateEntry[2]
-          const quantity = +updateEntry[3]
-
-          orderBookUpdate[orderSide].push({
-            type: quantity > 0 ? 2 : 1,
-            rate: rate,
-            quantity: quantity
-          })
-        }
-
-        this.markets[market].onUpdateExchangeState(orderBookUpdate)
+      for (const rate in asks) {
+        orderBook.asks.push({
+          rate: +rate,
+          quantity: +asks[rate]
+        })
       }
+
+      for (const rate in bids) {
+        orderBook.bids.push({
+          rate: +rate,
+          quantity: +bids[rate]
+        })
+      }
+
+      this.markets[market].onInitialState(orderBook)
+    }
+  }
+
+  private onOrderUpdate (market: MarketName, payload: Array<OrderUpdate>): void {
+    if (this.haveMarket(market)) {
+      const orderBookUpdate: OrderBookStateUpdate = { asks: [], bids: [] }
+
+      for (const updateEntry of payload) {
+        if (updateEntry[0] !== 'o') {
+          // might be 't' for trades
+          continue
+        }
+        const orderSide = +updateEntry[1] === 0 ? 'asks' : 'bids'
+        const rate = +updateEntry[2]
+        const quantity = +updateEntry[3]
+
+        orderBookUpdate[orderSide].push({
+          type: quantity > 0 ? 2 : 1,
+          rate: rate,
+          quantity: quantity
+        })
+      }
+
+      this.markets[market].onUpdateExchangeState(orderBookUpdate)
     }
   }
 
