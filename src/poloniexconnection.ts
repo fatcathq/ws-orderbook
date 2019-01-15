84e8e68cb3b8c3c3a0fbc13be33ee159938c5fc0
Themis Papameletiou <themicp@gmail.com>
2019-01-15 22:12:17 +0200

    [POLONIEX_CONNECTION]: Reconnect in case of connection error

--- src/poloniexconnection.ts
+++ src/poloniexconnection.ts
@@ -20,11 +20,13 @@ export default class PoloniexConnection extends Connection {
     if (this.client) {
       logger.debug('[POLONIEX]: Closing previous connection')
       this.client.off('message', this.onMessage)
+      this.client.off('error', this.connectionDied)
       this.client.close()
     }
 
     logger.debug('[POLONIEX]: Openning new connection')
     this.client = new WebSocket('wss://api2.poloniex.com')
+    this.client.on('error', this.connectionDied)
     this.client.on('open', () => {
       logger.debug('[POLONIEX]: Connection opened')
       this.subscriptions.forEach(channel => {
@@ -57,16 +59,18 @@ export default class PoloniexConnection extends Connection {
     this.emit('updateExchangeState', JSON.parse(messageString))
   }
 
+  private connectionDied = (): void => {
+    logger.warn('[POLONIEX]: Connection died')
+    this.isConnected = false
+    this.connect()
+  }
+
   private alive (): void {
     logger.debug('[POLONIEX]: Connection alive')
     if (this.aliveTimeout) {
       clearTimeout(this.aliveTimeout)
     }
 
-    this.aliveTimeout = setTimeout(() => {
-      logger.warn('[POLONIEX]: Connection died')
-      this.isConnected = false
-      this.connect()
-    }, HEARTBEAT_TIMEOUT_MS)
+    this.aliveTimeout = setTimeout(this.connectionDied, HEARTBEAT_TIMEOUT_MS)
   }
 }
