4479708d8b321efc6ed7478e9fcba4f6b6c24786
Dimitris Lamprinos <pkakelas@gmail.com>
2019-03-15 15:46:07 +0200

    Add onInitialState

--- src/cobinhoodstreamer.ts
+++ src/cobinhoodstreamer.ts
@@ -1,6 +1,11 @@
 import Streamer from './streamer'
-import CobinhoodConnection/*, { CobinhoodConnectionTypes }*/ from './cobinhoodconnection'
+import CobinhoodConnection, { CobinhoodConnectionTypes } from './cobinhoodconnection'
 import { MarketName } from './market'
+import { OrderBookState } from './orderbook'
+
+function cobinhoodPairToStandardPair (market: MarketName): MarketName {
+  return market.replace('-', '/')
+}
 
 export default class CobinhoodStreamer extends Streamer {
   constructor () { super('cobinhood') }
@@ -8,9 +13,39 @@ export default class CobinhoodStreamer extends Streamer {
   setupConn (): void {
     this.conn = new CobinhoodConnection()
 
-    this.conn.on('updateExchangeState', (message: any) => {
-      console.log(message)
+    this.conn.on('updateExchangeState', (message: CobinhoodConnectionTypes.Message) => {
+      const marketName = cobinhoodPairToStandardPair(message.h[0].split('.')[1])
+
+      if (message.h['2'] == 's') {
+        this.onInitialState(marketName, message.d)
+      }
+
+      if (message.h['2'] == 'u') {
+      }
     })
+  }
+
+  private onInitialState (market: MarketName, payload: CobinhoodConnectionTypes.OrderBookData): void {
+    if (this.haveMarket(market)) {
+      const {asks, bids} = payload
+      const orderBook: OrderBookState = { asks: [], bids: [] }
+
+      for (const entry in asks) {
+        orderBook.asks.push({
+          rate: Number(entry[0]),
+          quantity: Number(entry[2])
+        })
+      }
+
+      for (const entry in bids) {
+        orderBook.bids.push({
+          rate: Number(entry[0]),
+          quantity: Number(entry[2])
+        })
+      }
+
+      this.markets[market].onInitialState(orderBook)
+    }
   }
 
   subscribeToMarket (market: MarketName): Promise<void> {
