fbfbc7d67cb360efd6436dcb1a14c676fb1d655b
Themis Papameletiou <themicp@gmail.com>
2019-04-04 19:00:17 +0300

    Refactor Cryptowatch connection

--- src/cryptowatchconnection.ts
+++ src/cryptowatchconnection.ts
@@ -1,4 +1,3 @@
-import WebSocket from 'ws'
 import logger from 'logger'
 import Connection from './connection'
 import axios from 'axios'
@@ -18,29 +17,16 @@ export namespace CryptowatchConnectionTypes {
 
 export default class CryptowatchConnection extends Connection {
   private client!: any
-  // private subscriptions: Set<string> = new Set()
-  // private channels: Map<number, string> = new Map()
+  private cachedMarkets: Map<string, Map<string, CryptowatchConnectionTypes.Market>> | null = null
+  private subscriptions: Set<string> = new Set()
+  private subscribedMarketIds: Map<number, string> = new Map()
+  private lastSeqNum: Map<number, number> = new Map()
 
-  constructor () { super('cryptowatch', HEARTBEAT_TIMEOUT_MS) }
+  constructor (private readonly exchange: string) { super(`cryptowatch`, HEARTBEAT_TIMEOUT_MS) }
 
   protected disconnect (): Promise<void> {
-    if (this.client && this.client.readyState !== WebSocket.CLOSED) {
-      logger.debug('[CW]: Closing previous connection')
-
-      return new Promise((resolve) => {
-        try {
-          this.client.removeEventListener('message')
-          this.client.removeEventListener('off')
-          this.client.on('close', resolve)
-          this.client.close()
-        } catch (err) {
-          logger.debug(err.message, err)
-
-          this.client.off('close', resolve)
-          resolve()
-        }
-      })
-    }
+    this.client.disconnect()
+    this.lastSeqNum = new Map()
 
     return Promise.resolve()
   }
@@ -48,100 +34,97 @@ export default class CryptowatchConnection extends Connection {
   protected connect (): void {
     this.client = new CWStreamClient({
       apiKey: 'UXU4C4S93SPA0H4YSAPO',
-      secretKey: 'uT0iZo9XF0RMmD1MUGjVMOCwEc0qjTPc48qp73fB',
-      subscriptions: [
-        'markets:261:book:snapshots',
-        'markets:261:book:deltas'
-      ]
+      secretKey: 'uT0iZo9XF0RMmD1MUGjVMOCwEc0qjTPc48qp73fB'
     })
     this.client.connect()
 
     logger.debug('[CW]: Openning new connection')
-    // this.client.onError(this.refreshConnection.bind(this, 'connectionerror'))
+    this.client.onError((err: any) => {
+      if (err !== ERROR.PROTOBUF) {
+        logger.warn(`[CW]: Connection error: ${err}`)
+        this.refreshConnection(`connectionerror`)
+      }
+    })
     this.client.onConnect(() => {
       logger.debug('[CW]: Connection opened')
-        /*
       this.subscriptions.forEach(pair => {
-        logger.debug(`[KRAKEN]: Resubscribing to ${pair}`)
+        logger.debug(`[CW]: Resubscribing to ${pair}`)
         this.subscribe(pair)
       })
-         */
 
       this.alive()
       this.connectionOpened()
     })
-    // this.client.conn.on('message', this.handleMessage.bind(this))
-    this.client.onMarketUpdate((marketData: any) => {
-      this.emit('updateExchangeState', marketData)
-    })
+    this.client.onMarketUpdate(this.onMessage)
   }
 
-  subscribe (pair: string): Promise<void> {
-    logger.debug(`[CW]: Subscribing to pair ${pair}`)
-    /*
+  private onMessage = (marketData: any): void => {
+    const marketId = +marketData.market.marketId
+    const market = this.subscribedMarketIds.get(marketId)
+
+    if (marketData.orderBookDeltaUpdate) {
+      const currentSeqNum = marketData.orderBookDeltaUpdate.seqNum
+      const marketLastSeqNum = this.lastSeqNum.get(marketId)
+
+      if (typeof marketLastSeqNum === 'undefined') {
+        this.lastSeqNum.set(marketId, currentSeqNum)
+      } else if (marketLastSeqNum !== currentSeqNum - 1) {
+        logger.error(`Invalid sequence number for ${market}. Previous: ${marketLastSeqNum}. Current: ${currentSeqNum}`)
+        this.refreshConnection('seqnum')
+        return
+      }
+      this.lastSeqNum.set(marketId, currentSeqNum)
+    }
+
+    this.emit('updateExchangeState', marketData, market)
+  }
+
+  private async markets (): Promise<Map<string, Map<string, CryptowatchConnectionTypes.Market>>> {
+    if (!this.cachedMarkets) {
+      this.cachedMarkets = new Map()
+      const markets: Array<CryptowatchConnectionTypes.Market> = (await axios.get('https://api.cryptowat.ch/markets')).data.result
+
+      for (const market of markets) {
+        if (!this.cachedMarkets.has(market.exchange)) {
+          this.cachedMarkets.set(market.exchange, new Map())
+        }
+        const exchange = this.cachedMarkets.get(market.exchange)
+        if (typeof exchange === 'undefined') {
+          throw new Error(`Could not find markets for ${exchange}`)
+        }
+
+        exchange.set(market.pair, market)
+        this.cachedMarkets.set(market.exchange, exchange)
+      }
+    }
+
+    return this.cachedMarkets
+  }
+
+  async subscribe (pair: string): Promise<void> {
+    const markets = await this.markets()
+    const exchangeMarkets = markets.get(this.exchange)
+    if (typeof exchangeMarkets === 'undefined') {
+      throw new Error(`No markets found for ${this.exchange}`)
+    }
+
+    const market = exchangeMarkets.get(pair)
+    if (typeof market === 'undefined') {
+      throw new Error(`No market data for ${pair}`)
+    }
+
     this.subscriptions.add(pair)
-    this.client.send(JSON.stringify({
-      event: 'subscribe',
-      pair: [pair],
-      subscription: { name: 'book' }
-    }))
-     */
+    this.subscribedMarketIds.set(market.id, pair)
+    logger.debug(`[CW]: Subscribing to pair ${pair} with id ${market.id}`)
+    this.client.subscribe([
+      `markets:${market.id}:book:snapshots`,
+      `markets:${market.id}:book:deltas`
+    ])
 
     return Promise.resolve()
   }
 
   call (): Promise<void> {
-    throw new Error('[KRAKEN]: call() not implemented')
+    throw new Error('[CW]: call() not implemented')
   }
-
-    /*
-  private onMessage = (messageString: string): void => {
-    this.alive()
-    const message = JSON.parse(messageString)
-    if (message.event) {
-      logger.debug(`[KRAKEN]: Received ${message.event}`)
-      if (message.event === 'subscriptionStatus') {
-        this.channels.set(message.channelID, message.pair)
-      } else if (message.event === 'systemStatus' && message.status !== 'online') {
-        logger.warn(`[KRAKEN]: Received system status ${message.status}`)
-      }
-      return
-    }
-
-    message.splice(1, 0, this.channels.get(message[0]))
-    this.emit('updateExchangeState', message)
-  }
-  private handleMessage (data: any): void {
-      // Heartbeat
-    const bytes = new Uint8Array(data)
-    if (bytes.length === 1 && bytes[0] === 1) {
-      return
-    }
-
-    let message
-    try {
-      message = StreamMessage.decode(data)
-    } catch (e) {
-      console.log(e)
-      return
-    }
-
-    console.log(message.body)
-    switch (message.body) {
-      case 'authenticationResult':
-        this.client.authResultHandler(message.authenticationResult)
-        break
-      case 'marketUpdate':
-        console.log('market update')
-        console.log(message.marketUpdate.orderBookUpdate)
-        break
-      case 'pairUpdate':
-        console.log('pair update')
-        console.log(message.pairUpdate)
-        break
-      default:
-        console.log('error')
-    }
-  }
-    */
 }
