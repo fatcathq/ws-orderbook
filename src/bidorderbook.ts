184b0bf05d0455989a3c8c701fc076eebce89239
Themis Papameletiou <themicp@gmail.com>
2019-05-17 18:19:36 +0300

    [ORDERBOOK]: Find the best rate and return it without sorting the whole orderbook if limit=1

--- src/bidorderbook.ts
+++ src/bidorderbook.ts
@@ -5,6 +5,10 @@ class BidOrderBook extends OrderBook {
   top = (limit = 1): Array<OrderBookRecord> => {
     const rates = Object.keys(this.store).map(k => this.store[k].rate)
 
+    if (limit === 1) {
+      const bestRate = Math.max(...Object.keys(this.store).map(rate => +rate))
+      return [this.store[bestRate.toString()]]
+    }
     rates.sort(decreasingOrderCompare)
     rates.splice(limit)
 
